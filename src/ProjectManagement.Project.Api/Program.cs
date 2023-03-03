959d9a160dd53dbcd2b1dc991f8005aedaba7a7c
afroze9 <afrozeamjad@gmail.com>
2023-03-03 14:07:02 +0500

    Consul works without security

--- src/ProjectManagement.Project.Api/Program.cs
+++ src/ProjectManagement.Project.Api/Program.cs
@@ -1,3 +1,4 @@
+using Steeltoe.Discovery.Client;
 
 namespace ProjectManagement.Project.Api;
 
@@ -6,9 +7,11 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
+        
+        builder.Configuration.AddJsonFile("hostsettings.json", optional: true);
+        
         // Add services to the container.
-
+        builder.Services.AddDiscoveryClient();
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
